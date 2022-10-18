b7279b69726ad491041739060e7356b9aaba503a
sammi12006 <sammi12006@gmail.com>
2022-10-18 12:58:56 -0600

    Fixed apos

--- components/Hero.js
+++ components/Hero.js
@@ -10,8 +10,8 @@ const Hero = () => {
             <div className="pr-lg-5">
               {/* <p className="text-uppercase text-primary font-weight-medium f-14 mb-4"></p> */}
               <h1 className="mb-4 font-weight-normal line-height-1_4">The future of social cannabis consumption is <span className="important-text font-weight-medium">coming soon...</span></h1>
-              <p className="text-muted mb-4 pb-2">For too long, cannabis consumers have been fed marketing fluff and false flags about what's <b>really good</b> with the industry </p> 
-              <p className="text-muted mb-4">We're here to change that. </p>
+              <p className="text-muted mb-4 pb-2">For too long, cannabis consumers have been fed marketing fluff and false flags about what&apos;s <b>really good</b> with the industry </p> 
+              <p className="text-muted mb-4">We&apos;re here to change that. </p>
               <p className="text-muted mb-4 pb-2">
               As the rest of the country prepares for legalization, these culture vultures will only inrease their grip upon the industry - fight back! <b>Join the revolution</b> and help us build a better future for cannabis consumers everywhere.
               </p>
