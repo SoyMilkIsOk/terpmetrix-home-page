57d577a862a2a92fa1805f12fe7fd24f8dd54598
sammi12006 <sammi12006@gmail.com>
2023-01-28 12:19:55 -0700

    Added whitepaper - made home simpler

--- components/Hero.js
+++ components/Hero.js
@@ -10,13 +10,16 @@ const Hero = () => {
             <div className="pr-lg-5">
               {/* <p className="text-uppercase text-primary font-weight-medium f-14 mb-4"></p> */}
               <h1 className="text-light mb-4 font-weight-normal line-height-1_4">The future of cannabis analytics is <span className="important-text font-weight-medium">coming soon...</span></h1>
-              <p className="text-light mb-4 pb-2">For too long, the entire cannabis industry has been fed marketing fluff and false information, struggling to find what&apos;s <b>really good</b> in the industry.</p>
-              <p className="text-light mb-4 pb-2">Slowly but surely, the industry is coming to realize THC isn&apos;t the only thing that matters. <span className="important-text font-weight-medium">Terpenes</span> are quickly becoming the driving factor when it comes to purchasing cannabis.</p>
-              <p className="text-light mb-4 pb-2">We&apos;re here to provide <i> accurate</i>, <i>on-demand</i> terpene testing for everyone ranging from home-growers to the biggest commercial grows. 
-              </p>
-              <p className="text-light mb-2">
+              <p className="text-light mb-4 pb-1">Slowly but surely, the industry is coming to realize THC isn&apos;t the only thing that matters. <span className="important-text font-weight-medium">Terpenes</span> are quickly becoming the driving factor when it comes to purchasing cannabis.</p>
+              <p className="text-light mb-2 pb-2">
               <b>Join us </b> in building a better future for cannabis everywhere. 
               </p>
+
+              {/* button for whitepaper */}
+              <button>
+                <a className='text-light' href='/Terpmetrix-WhitePaper.pdf'>Read Whitepaper <span className="ml-2 right-icon">&#8594;</span></a>
+              </button>
+              <p className='pb-4'> </p>
 
               <form className="Form" name="beta-users" data-netlify="true">
               <input type="hidden" name="form-name" value="beta-users" />
