431d20235c475ad6ee0ed38111d9232763c3c336
Sam S <sammi12006@gmail.com>
2023-01-31 13:09:43 -0700

    Update sheit

--- components/Hero.js
+++ components/Hero.js
@@ -11,15 +11,16 @@ const Hero = () => {
               {/* <p className="text-uppercase text-primary font-weight-medium f-14 mb-4"></p> */}
               <h1 className="text-light mb-4 font-weight-normal line-height-1_4">The future of cannabis is <span className="important-text font-weight-medium">coming soon...</span></h1>
               <p className="text-light mb-4 pb-1">Slowly but surely, the industry is coming to realize THC isn&apos;t the only thing that matters. <span className="important-text font-weight-medium">Terpenes</span> are quickly becoming the driving factor when it comes to purchasing cannabis.</p>
-              <p className="text-light mb-2 pb-2">
-              <b>Join us </b> in building a better future for cannabis everywhere. 
-              </p>
 
               {/* button for whitepaper */}
               <button>
                 <a className='text-light' href='/Terpmetrix-WhitePaper.pdf'>Read Whitepaper <span className="ml-2 right-icon">&#8594;</span></a>
               </button>
-              <p className='pb-4'> </p>
+              <p className='pb-2'> </p>
+
+              <p className="text-light mb-2 pb-2">
+              <b>Join us </b> in building a better future for cannabis everywhere. 
+              </p>
 
               <form className="Form" name="beta-users" data-netlify="true">
               <input type="hidden" name="form-name" value="beta-users" />
@@ -46,7 +47,7 @@ const Hero = () => {
           <span className='emojis peach'>🍑</span>
           <span className='emojis grape'>🍇</span>
           <span className='emojis cherry'>🍒</span>
-          <span className='emojis blueberry'>🫐</span>
+          <span className='emojis wood'>🪵</span>
       </div>
     </section>
   );
