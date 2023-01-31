8775dbce303b73c5ef913347e8ecf2911728f335
Sam S <sammi12006@gmail.com>
2023-01-30 22:15:17 -0700

    Update BG

--- components/Hero.js
+++ components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
           <Col lg={6}>
             <div className="pr-lg-5">
               {/* <p className="text-uppercase text-primary font-weight-medium f-14 mb-4"></p> */}
-              <h1 className="text-light mb-4 font-weight-normal line-height-1_4">The future of cannabis analytics is <span className="important-text font-weight-medium">coming soon...</span></h1>
+              <h1 className="text-light mb-4 font-weight-normal line-height-1_4">The future of cannabis is <span className="important-text font-weight-medium">coming soon...</span></h1>
               <p className="text-light mb-4 pb-1">Slowly but surely, the industry is coming to realize THC isn&apos;t the only thing that matters. <span className="important-text font-weight-medium">Terpenes</span> are quickly becoming the driving factor when it comes to purchasing cannabis.</p>
               <p className="text-light mb-2 pb-2">
               <b>Join us </b> in building a better future for cannabis everywhere. 
@@ -35,13 +35,19 @@ const Hero = () => {
 
             </div>
           </Col>
-          <Col lg={6} className="grid-pic">
-            <div className="mt-5 mt-lg-0 ms-4">
-              <img src="/images/people-v3.png" alt="" className="img-fluid mx-auto d-block"/>
-            </div>
-          </Col>
         </Row>
       </Container>
+      <div className="mt-5 mt-lg-0 ms-4 bgimage">
+          <span className='emojis fire'>🔥</span>
+          <span className='emojis gas'>⛽️</span>
+          <span className='emojis leaf'>🌿</span>
+          <span className='emojis lemon'>🍋</span>
+          <span className='emojis orange'>🍊</span>
+          <span className='emojis peach'>🍑</span>
+          <span className='emojis grape'>🍇</span>
+          <span className='emojis cherry'>🍒</span>
+          <span className='emojis blueberry'>🫐</span>
+      </div>
     </section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Feature from '../components/Feature';
-import Service from '../components/Service';
-import About from '../components/About';
 import Footer from '../components/Footer';
-import AccordianDropdown from "../components/AccordianDropdown";
 
 const Index = () => {
   return (
-    <Layout pageTitle="The Future of Cannabis Analytics | Terpmetrix">
+    <Layout pageTitle="The Future of Cannabis - Terpmetrix">
       <Header />
       <Hero />
       <Footer />
