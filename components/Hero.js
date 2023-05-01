4d882c713e0bbd9890f820f1e9992e463280d399
Sam S <sammi12006@gmail.com>
2023-05-01 12:52:41 -0600

    Upddate homepage

--- components/Hero.js
+++ components/Hero.js
@@ -10,28 +10,30 @@ const Hero = () => {
             <div className="pr-lg-5">
               {/* <p className="text-uppercase text-primary font-weight-medium f-14 mb-4"></p> */}
               <h1 className="text-light mb-4 font-weight-normal line-height-1_4">The future of cannabis is <span className="important-text font-weight-medium">coming soon...</span></h1>
-              <p className="text-light mb-4 pb-1">Slowly but surely, the industry is coming to realize THC isn&apos;t the only thing that matters. <span className="important-text font-weight-medium">Terpenes</span> are quickly becoming the driving factor when it comes to purchasing cannabis.</p>
+
+              <p className="text-light mb-4 pb-1">
+                Welcome to <span className="important-text font-weight-medium">Terpmetrix</span>, the pioneering force in the cannabis industry dedicated to unlocking the full potential of terpenes. Our innovative technology is revolutionizing the way consumers explore, understand, and experience cannabis.
+              </p>
 
               {/* button for whitepaper */}
-              <button>
+              {/* <button>
                 <a className='text-light' href='/Terpmetrix-WhitePaper.pdf'>Read Whitepaper <span className="ml-2 right-icon">&#8594;</span></a>
-              </button>
-              <p className='pb-2'> </p>
+              </button> */}
 
-              <p className="text-light mb-2 pb-2">
-              <b>Join us </b> in building a better future for cannabis everywhere. 
+              <p className="text-light mb-4 pb-1">
+Sign up to learn more about our groundbreaking solutions, receive updates on our latest advancements, and be part of the future of cannabis.
               </p>
 
               <form className="Form" name="beta-users" data-netlify="true">
-              <input type="hidden" name="form-name" value="beta-users" />
-              <p>
-              <label><input name ="email" type="email" placeholder="Enter your email" /></label>
-              </p>
-              <p>
-              <button type="submit" className='text-light'>
-              Get Updates <span className="ml-2 right-icon">&#8594;</span>
-              </button>
-              </p>
+                <input type="hidden" name="form-name" value="beta-users" />
+                <p>
+                  <label><input name="email" type="email" placeholder="Enter your email" /></label>
+                </p>
+                <p>
+                  <button type="submit" className='text-light'>
+                    Get Updates <span className="ml-2 right-icon">&#8594;</span>
+                  </button>
+                </p>
               </form>
 
             </div>
@@ -39,15 +41,15 @@ const Hero = () => {
         </Row>
       </Container>
       <div className="mt-5 mt-lg-0 ms-4 bgimage">
-          <span className='emojis fire'>🔥</span>
-          <span className='emojis gas'>⛽️</span>
-          <span className='emojis leaf'>🌿</span>
-          <span className='emojis lemon'>🍋</span>
-          <span className='emojis orange'>🍊</span>
-          <span className='emojis peach'>🍑</span>
-          <span className='emojis grape'>🍇</span>
-          <span className='emojis cherry'>🍒</span>
-          <span className='emojis wood'>🪵</span>
+        <span className='emojis fire'>🔥</span>
+        <span className='emojis gas'>⛽️</span>
+        <span className='emojis leaf'>🌿</span>
+        <span className='emojis lemon'>🍋</span>
+        <span className='emojis orange'>🍊</span>
+        <span className='emojis peach'>🍑</span>
+        <span className='emojis grape'>🍇</span>
+        <span className='emojis cherry'>🍒</span>
+        <span className='emojis wood'>🪵</span>
       </div>
     </section>
   );
