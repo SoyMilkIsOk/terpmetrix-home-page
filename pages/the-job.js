b73a62f629ee954ab6b19311dc941b5a0507d42d
Sam S <sammi12006@gmail.com>
2024-02-01 16:56:56 -0700

    The job

---
+++ pages/the-job.js
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Footer from '../components/Footer';
+
+const TheJob = () => {
+  return (
+    <Layout pageTitle="The Job @ Terpmetrix">
+      <Header />
+      <Hero />
+      <Footer />
+    </Layout>
+  )
+}
+
+export default TheJob;
