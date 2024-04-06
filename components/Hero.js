acd183720f82f5b5d2c6ce2358787606f75c3143
Sam S <sammi12006@gmail.com>
2024-04-06 10:33:24 -0600

    Test

--- components/Hero.js
+++ components/Hero.js
@@ -46,20 +46,14 @@ const Hero = () => {
                 data-netlify-recaptcha="true"
               >
                 <input type="hidden" name="form-name" value="beta-users" />
-                <p>
-                  <label>
                     <input
                       name="email"
                       type="email"
                       placeholder="Enter your email"
                     />
-                  </label>
-                </p>
-                <p>
                   <button type="submit" className="text-light">
                     Get Updates <span className="ml-2 right-icon">&#8594;</span>
                   </button>
-                </p>
               </form>
             </div>
           </Col>
