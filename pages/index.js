df04fcfd67b11589cc721703c1af618108ee03c1
Sam S <sammi12006@gmail.com>
2024-04-06 10:30:42 -0600

    remove from index

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,6 @@ import Footer from '../components/Footer';
 const Index = () => {
   return (
     <Layout pageTitle="The Future of Cannatech - Terpmetrix">
-      <form name="beta-users" netlify netlify-honeypot="bot-field" action='/' hidden>
-  <input type="email" name="email" />
-</form>
-<form name="maikoh-entries-1" netlify netlify-honeypot="bot-field" action='/' hidden>
-  <input type="text" name="username" />
-</form>
       <Header />
       <Hero />
       <Footer />
