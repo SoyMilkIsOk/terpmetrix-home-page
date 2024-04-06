51dad9636b4718d2019fa229dcfb0df7ce457407
Sam S <sammi12006@gmail.com>
2024-04-06 10:59:48 -0600

    Submit page

--- pages/success.js
+++ pages/success.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
 const SuccessPage = () => {
-  const history = useHistory();
-
-  const handleGoBack = () => {
-    history.goBack();
-  };
 
   return (
     <div>
       <h1>Success!</h1>
       <p>Your form has been submitted successfully.</p>
-      <button onClick={handleGoBack}>Go Back</button>
     </div>
   );
 };
